5a2ca60bca669b1e7ed20e4688ffd7e4963d577e
Elon61 <Elon61@users.noreply.github.com>
2018-01-05 16:27:18 +0200

    Heh

--- bin_stuff.py
+++ bin_stuff.py
@@ -8,13 +8,27 @@ class BinBuilder(object):
         pass
 
     def straight(self, spin):
-        for num in xrange(1, 37):
-            spin.addOutcome(num, outcome.Outcome(num, 35))
+        for num in xrange(0, 37):
+            spin.addOutcome(num, outcome.Outcome(str(num), 35))
         spin.addOutcome(37, outcome.Outcome("00", 35))
-        spin.addOutcome(0, outcome.Outcome("0", 35))
 
-    def split(self):
-        pass
+    def split(self, spin):
+        for num in xrange(1, 37):
+            if num % 3 == 0:
+                #all the right row
+                spin.addOutcome(num, outcome.Outcome("{}-{}".format(num, num + 1), 17))
+
+            if (num - 1) % 3 == 0:
+                #all the left row
+                spin.addOutcome(num, outcome.Outcome("{}-{}".format(num, num - 1), 17))
+
+            if num > 3:
+                #all but 1, 2 and 3
+                spin.addOutcome(num, outcome.Outcome("{}-{}".format(num, num - 3), 17))
+
+            if num < 34:
+                #all but 34, 35, 36
+                spin.addOutcome(num, outcome.Outcome("{}-{}".format(num, num + 3), 17))
 
     def street(self):
         pass
