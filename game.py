0a1bf31d850b8925f86462d4288d30d36ce06463
Elon61 <Elon61@users.noreply.github.com>
2018-03-04 17:36:28 +0200

    Heh

--- game.py
+++ game.py
@@ -8,16 +8,25 @@ class Game(object):
         self._table = table.Table()
 
     def _fetch_bets(self):
-        for p in self._players:
-            bet = p.new_bet()
-            self._bets2players[bet] = p
+        for players in self._players:
+            bet = players.new_bet()
+            self._bets2players[bet] = players
 
     def _put_bets_on_table(self):
-        for t in self._bets2players:
-            self._table.add_bet(t)
+        for bet in self._bets2players:
+            self._table.add_bet(bet)
+
+    def _notify_players(self, wb, lb):
+        for bet in wb:
+            self._bets2players[bet].won_bet(bet)
+        for bet in lb:
+            self._bets2players[bet].lost_bet(bet)
 
     def play_round(self):
-        self.table
+        self._table.reset()
+        self._bets2players = {}
+        self._fetch_bets()
+        self._put_bets_on_table()
         wowtcome = self._spin_It.spin()
         winning_bets = self._table.get_winning_bets(wowtcome)
         losing_bets = self._table.get_losing_bets(wowtcome)
--- spin.py
+++ spin.py
@@ -3,4 +3,3 @@ import outcome
 import bin_stuff
 banana = spinner.Spinner()
 print banana.spin()
-print banana.spin()
